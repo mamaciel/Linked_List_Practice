4299e49463af1ecd49110e3872ec079a90f91ddf
mamaciel <marcospaulofilho@hotmail.com>
2022-10-13 02:07:56 -0400

    Fixed deleting last node, added update() and size()

--- MyOwnLinkedList.py
+++ MyOwnLinkedList.py
@@ -46,7 +46,7 @@ class LList():
         else:   
             
             while current.val != val:
-                current.prev = current
+                current_prev = current
                 current = current.next
             
             #delete middle
@@ -56,10 +56,32 @@ class LList():
             
             #delete tail
             elif current.next == None:
-                current.val = None
+                current = current_prev
+                current.next = None
                 
-        return "Not found"
+        return
+    
+    def update(self, idx, val):
+        current = self.head
+        index = 0
+        if idx > self.size()-1:
+            print("Index is out of range, please try again")
+            return
         
+        while idx != index:
+            current = current.next
+            index += 1
+        
+        current.val = val
+            
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            current = current.next
+            count += 1
+        return count
+            
     def printList(self):
         lst = []
         current = self.head
@@ -68,6 +90,7 @@ class LList():
             lst.append(current.val)
             current = current.next
         
+        #Used enumerate so we know which node is last
         for i, j in enumerate(lst):
             if i == len(lst)-1:
                 print(f'{j}')
@@ -79,24 +102,30 @@ class LList():
 def main():
     MyList = LList()
     MyList.delete(1)
-    MyList.add(8)
-    MyList.printList()
-    MyList.add(9)
+    MyList.add(1)
+    #MyList.printList()
+    MyList.add(2)
     MyList.search(1)
-    MyList.printList()
+    #MyList.printList()
     MyList.delete(8)
-    MyList.printList()
-    MyList.add(10)
-    MyList.printList()
-    MyList.add(13)
-    MyList.add(15)
-    MyList.add(20)
-    MyList.add(5)
+    #MyList.printList()
     MyList.add(3)
+    #MyList.printList()
+    MyList.add(4)
+    MyList.add(5)
     MyList.add(6)
-    MyList.delete(15)
-    MyList.printList()
-    MyList.delete()
+    MyList.add(7)
+    MyList.add(8)
+    MyList.add(9)
+    #MyList.printList()
+    MyList.delete(5)
+    #MyList.printList()
+    MyList.delete(9)
+    #MyList.printList()
+    MyList.add(10)
+    #MyList.printList()
+    MyList.update(8, 99)
+    MyList.update(5, 99)
     MyList.printList()
 
 if __name__ == "__main__":
