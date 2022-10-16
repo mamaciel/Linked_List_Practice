6115f4f7ac6a9d2d3ab7f1b492d179df70416dc2
mamaciel <marcospaulofilho@hotmail.com>
2022-10-16 19:01:58 -0400

    Adjusted return statements and if/else statements

--- MyOwnLinkedList.py
+++ MyOwnLinkedList.py
@@ -2,13 +2,14 @@ class Node():
     def __init__(self, val, next = None):
         self.val = val
         self.next = next
-        
+
 class LList():
     def __init__(self):
         self.head = None
-        
+
     def add(self, val):
         newNode = Node(val)
+        
         if not self.head:
             self.head = newNode
             
@@ -18,8 +19,9 @@ class LList():
             while current.next:
                 current = current.next
             current.next = newNode
-        return
             
+        return
+
     def search(self, val):
         current = self.head
         while current:
@@ -27,24 +29,25 @@ class LList():
                 return True
             else:
                 current = current.next
+                
         return False
-    
+
     def delete(self, val):
         if not self.head:
             print("This linked list is empty, there is nothing to delete.")
             return
         
-        elif not self.search(val):
+        if not self.search(val):
             print(f"The value \'{val}\' was not found in the linked list.")
             return
-        
+
         #delete head
         current = self.head
         if current.val == val:
             self.head = current.next
-        
+
         else:   
-            
+
             while current.val != val:
                 current_prev = current
                 current = current.next
@@ -55,33 +58,37 @@ class LList():
                 current.next = current.next.next
             
             #delete tail
-            elif current.next == None:
+            else:
                 current = current_prev
                 current.next = None
-                
+            
         return
-    
+
     def update(self, idx, val):
         current = self.head
         index = 0
         if idx > self.size()-1:
             print("Index is out of range, please try again")
-            return
         
-        while idx != index:
-            current = current.next
-            index += 1
-        
-        current.val = val
+        else:
+            while idx != index:
+                current = current.next
+                index += 1
             
+            current.val = val
+            
+        return
+
     def size(self):
         current = self.head
         count = 0
+        
         while current:
             current = current.next
             count += 1
-        return count
             
+        return count
+    
     def printList(self):
         lst = []
         current = self.head
@@ -94,11 +101,12 @@ class LList():
         for i, j in enumerate(lst):
             if i == len(lst)-1:
                 print(f'{j}')
-                return
+            
             else:
                 print(f'{j} -> ', end = " ")
+                
         return
-    
+
 def main():
     MyList = LList()
     MyList.delete(1)
@@ -126,6 +134,7 @@ def main():
     #MyList.printList()
     MyList.update(8, 99)
     MyList.update(5, 99)
+    MyList.delete(10)
     MyList.printList()
 
 if __name__ == "__main__":
